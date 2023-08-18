bb2f95af0ccaaf9dcfcbf1e964c3762c6f87bedd
x9dragon <141993179+x9dragon@users.noreply.github.com>
2023-08-18 16:20:20 -0500

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -23,12 +23,6 @@ _4.- La "empresa" no se hace responsable del uso de un Sub Bot o Bot no oficial,
 _5.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
 _6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de The Mystic - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
 
-*➤ Mensaje del numero del Bot?*
-_- Si en algún momento recibe un mensaje del numero del Bot y no es un comando posiblemente sea de el propietario del Bot o el propietario del numero, este es un persona real por lo que se le pide que no le falte el respeto_
-
-*➤ Base usada para este Bot:*
-*- https://github.com/BochilGaming/games-wabot-md*
-
 *➤ Agradecimientos a:*
 *- Unx21*
 *- Narutomo*
@@ -44,13 +38,4 @@ _- Si en algún momento recibe un mensaje del numero del Bot y no es un comando 
 *- FatihArridho*
 *- Adiwajshing*
 *- Darlyn*
-*- Etc.*
-
-*⁉️ Te quedaron dudas?*
-_- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
-
-> wa.me/5219992095479
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
-
-*∆ THE MYSTIC - BOT - MD ∆*`;
+*- Etc.*`;
