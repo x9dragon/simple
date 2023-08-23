cda19a08e1c3cc616a053a4f6e65052cd9806a04
x9dragon <141993179+x9dragon@users.noreply.github.com>
2023-08-23 16:13:53 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*𝐀𝐂𝐓𝐈𝐕𝐄𝐍 𝐏𝐋𝐀𝐍𝐓𝐀𝐒, 𝐓𝐄𝐍𝐄𝐌𝐎𝐒 𝐕𝐄𝐑𝐒𝐔𝐒*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
     teks += `🪴➤ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└*  👑 @bad._.fantasy👑\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* SOY UN 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
